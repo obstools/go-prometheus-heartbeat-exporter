e3706cddd5e4955a5f1ff6d79e8cdded46430b34
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-09-14 21:05:48 +0200

    Feature/Implement configuration ENV vars interpolation
    * Added interpolateEnvVars(), tests
    * Updated go.mod

--- configuration_test.go
+++ configuration_test.go
@@ -1,12 +1,26 @@
 package heartbeat
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestInterpolateEnvVars(t *testing.T) {
+	defer os.Unsetenv("TEST_ENV")
+
+	t.Run("when ENV var found in the string", func(t *testing.T) {
+		targetValue := "some_text"
+		os.Setenv("TEST_ENV", targetValue)
+
+		assert.Equal(t, "abc"+targetValue+"def", interpolateEnvVars("abc${TEST_ENV}def"))
+	})
+}
+
 func TestLoadConfiguration(t *testing.T) {
+	defer os.Unsetenv("METRICS_ROUTE")
+
 	t.Run("when configuration file exists and valid", func(t *testing.T) {
 		expectedConfiguration := &Configuration{
 			LogToStdout:  true,
@@ -29,11 +43,44 @@ func TestLoadConfiguration(t *testing.T) {
 		assert.Nil(t, err)
 	})
 
+	t.Run("when configuration file exists and valid", func(t *testing.T) {
+		additionalRoute := "/some_path"
+		os.Setenv("METRICS_ROUTE", additionalRoute)
+
+		expectedConfiguration := &Configuration{
+			LogToStdout:  true,
+			LogActivity:  true,
+			Port:         8080,
+			MetricsRoute: "/metrics" + additionalRoute,
+			InstancesAttributes: []*InstanceAttributes{
+				{
+					Name:        "postgres_1",
+					Connection:  "postgres",
+					URL:         "postgres://localhost:5432/healthcheck_db",
+					IntervalSec: 3,
+					TimeoutSec:  2,
+				},
+			},
+		}
+		actualConfiguration, err := loadConfiguration("./fixtures/config_with_env.yml")
+
+		assert.EqualValues(t, expectedConfiguration, actualConfiguration)
+		assert.Nil(t, err)
+	})
+
 	t.Run("when configuration file not exists", func(t *testing.T) {
-		configurationPath := "./fixtures/not_existing_config.yml"
+		configurationPath := "./fixtures/config_not_existing.yml"
 		actualConfiguration, err := loadConfiguration(configurationPath)
 
 		assert.EqualError(t, err, "open "+configurationPath+": no such file or directory")
+		assert.Nil(t, actualConfiguration)
+	})
+
+	t.Run("when configuration file is a broken file", func(t *testing.T) {
+		configurationPath := "./fixtures/config"
+		actualConfiguration, err := loadConfiguration(configurationPath)
+
+		assert.EqualError(t, err, "yaml: invalid trailing UTF-8 octet")
 		assert.Nil(t, actualConfiguration)
 	})
 
@@ -41,7 +88,15 @@ func TestLoadConfiguration(t *testing.T) {
 		configurationPath := "./fixtures"
 		actualConfiguration, err := loadConfiguration(configurationPath)
 
-		assert.EqualError(t, err, "yaml: input error: read "+configurationPath+": is a directory")
+		assert.EqualError(t, err, "read "+configurationPath+": is a directory")
+		assert.Nil(t, actualConfiguration)
+	})
+
+	t.Run("when configuration file is broken", func(t *testing.T) {
+		configurationPath := "./fixtures/config_broken.yml"
+		actualConfiguration, err := loadConfiguration(configurationPath)
+
+		assert.EqualError(t, err, "yaml: line 7: did not find expected key")
 		assert.Nil(t, actualConfiguration)
 	})
 }
