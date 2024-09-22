c77dde352c5ddb6d212b5675bfba23b1fa4d7553
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-09-22 16:56:15 +0200

    Feature/Session postgres
    * Added sessionPostgres(), tests
    * Updated CircleCI config

--- test_helpers_test.go
+++ test_helpers_test.go
@@ -1,8 +1,13 @@
 package heartbeat
 
 import (
+	"database/sql"
 	"regexp"
+
+	"github.com/lib/pq"
 )
+
+var testDB = "heartbeat_test"
 
 // Regex builder
 func newRegex(regexPattern string) (*regexp.Regexp, error) {
@@ -13,4 +18,39 @@ func newRegex(regexPattern string) (*regexp.Regexp, error) {
 func loggerMessageRegex(logLevel, logMessage string) *regexp.Regexp {
 	regex, _ := newRegex(logLevel + `: \d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\.\d{6} \[` + logMessage + "]")
 	return regex
+}
+
+// Postgres session
+// Create test database for postgres
+func createPostgresDb() error {
+	db, err := sql.Open("postgres", "host=localhost user=postgres port=5432 sslmode=disable")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE DATABASE " + testDB)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "42P04" {
+			return nil
+		}
+	}
+	return err
+}
+
+// Drop test database for postgres
+func dropPostgresDb() error {
+	db, err := sql.Open("postgres", "host=localhost user=postgres port=5432 sslmode=disable")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DROP DATABASE IF EXISTS " + testDB)
+	return err
+}
+
+// Compose connection string for postgres
+func composePostgresConnectionString() string {
+	return "postgres://postgres@localhost:5432/" + testDB
 }
