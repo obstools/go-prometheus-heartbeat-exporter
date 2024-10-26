dbc10ed25fa34748004440b2223c9fdf45638017
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-10-26 10:17:34 +0200

    Bugfix/Issue with package namespace (#23)
    * Fixed issue with package namespace
    * Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.2] - 2024-10-26
+
+### Fixed
+
+- Fixed issue with package namespace
+
 ## [0.1.1] - 2024-10-25
 
 ### Added
--- pkg/heartbeat/heartbeat_test.go
+++ pkg/heartbeat/heartbeat_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestNew(t *testing.T) {
 	t.Run("when configuration file is valid", func(t *testing.T) {
-		server, err := New("fixtures/config_without_instances.yml")
+		server, err := New("../../fixtures/config_without_instances.yml")
 
 		assert.NotNil(t, server)
 		assert.NoError(t, err)
 	})
 
 	t.Run("when configuration file is invalid", func(t *testing.T) {
-		server, err := New("fixtures/config_broken.yml")
+		server, err := New("../../fixtures/config_broken.yml")
 
 		assert.Nil(t, server)
 		assert.Error(t, err)
